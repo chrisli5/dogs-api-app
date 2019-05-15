53e7357b34d5943f3eebd400abef57fb72d1fac5
chrisli5 <chrisli41@gmail.com>
2019-05-14 19:09:19 -0700

    Add GET request by breed

--- index.js
+++ index.js
@@ -8,19 +8,34 @@ function getDogs(num) {
     fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
         .then(response => response.json())
         .then(responseJson => {
-            console.log(responseJson.message);
             displayImg(responseJson.message);
         })
         .catch(e => {
-            alert(`Error getting dogs!`);
+            alert(`Error getting dog by breed!`);
+        });
+}
+
+function getDogBreed(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+        .then(response => response.json())
+        .then(responseJson => {
+            $('#images').append(`<img src=${responseJson.message} alt="dog image" />`)
         });
 }
 
 function mainApp() {
-    $('form').submit(function(event) {
+    $('#dogForm').submit(function(event) {
         event.preventDefault();
-        const num = $('#text').val();
+        $('#images').empty();
+        const num = $('#number').val();
         getDogs(num);
+    })
+
+    $('#breedForm').submit(function(event) {
+        event.preventDefault();
+        $('#images').empty();
+        const breed = $('#text').val();
+        getDogBreed(breed);
     })
 }
 
